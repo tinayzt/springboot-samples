d6e9db1660cea26f5cc16131e9e06be08249a970
tinayzt <321182707@qq.com>
2026-03-05 21:29:19 +0800

    feat: add HelloController and ResultVO classes

---
+++ week01/springboot-hello/src/main/java/com/example/springboothello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.springboothello.controller;
+
+import com.example.springboothello.vo.ResultVO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResultVO<String> hello() {
+        return ResultVO.success("Hello Spring Boot 3.x!");
+    }
+}
---
+++ week01/springboot-hello/src/main/java/com/example/springboothello/vo/ResultVO.java
@@ -0,0 +1,56 @@
+package com.example.springboothello.vo;
+
+public class ResultVO<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public ResultVO() {
+    }
+
+    public ResultVO(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ResultVO<T> success(T data) {
+        return new ResultVO<>(200, "success", data);
+    }
+
+    public static <T> ResultVO<T> success(String message, T data) {
+        return new ResultVO<>(200, message, data);
+    }
+
+    public static <T> ResultVO<T> error(Integer code, String message) {
+        return new ResultVO<>(code, message, null);
+    }
+
+    public static <T> ResultVO<T> error(String message) {
+        return new ResultVO<>(500, message, null);
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
